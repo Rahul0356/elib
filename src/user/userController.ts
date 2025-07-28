d533dc7fa467d9a92c693edbc53bda6d7d65f814
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-07-28 07:29:58 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@ import  { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel"; 
 import  bcrypt from 'bcrypt'
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const  createUser= async(
     req:Request,
@@ -24,20 +27,55 @@ const  createUser= async(
         return next(error);
     }
     //Database
+
+    try{
+
     const user = await userModel  .findOne({email:email});
-    if(user){
-        const error = createHttpError(400,"USer already exits with this email");
+
+     if(user){
+        const error = createHttpError(
+        400,
+            "USer already exits with this email"
+        );
         return next(error);
 
     }
+    } catch(err){
+        return next(createHttpError(500,"Error while getting user"));
+
+    }
+
+    
+   
     //password --> hash
 
     const hashedPassword= await bcrypt.hash(password,10);
+    try{
+        
+        let newUser:User;
+
+     newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+}
+catch(err){
+    return next(createHttpError(500,"Error while creating user"))
+}
+
+    //Token generation
+    const token = sign({sub: newUser._id}, config.jwtSecret as string,
+        {
+            expiresIn:'7d',
+            algorithm:"HS256"
+        });
+
     // process
     //Response
 
 
-    res.json({message:"User created"})
+    res.json({accessToken:token});
 };
 
 export {createUser};
